8a87ca5acd4fcaab5176f7b948f86e728cf2189c
Matt Powell <fauxparse@gmail.com>
2016-02-06 13:46:23 +1300

    spec Team#to_param

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Team, type: :model do
+  subject(:team) { FactoryGirl.create(:team) }
+
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_presence_of(:slug) }
+
+  describe '#to_param' do
+    it 'returns the slug, not the ID' do
+      expect(team.to_param).to eq(team.slug)
+    end
+  end
 end
