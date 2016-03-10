b1773e7df45e7330498815ea7494235cb0b1ed67
Matt Powell <fauxparse@gmail.com>
2016-03-11 07:43:07 +1300

    take care of associated members when deleting users

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,18 @@ class User < ApplicationRecord
   has_many :teams, through: :members
 
   validates :name, presence: { allow_blank: false }
+
+  before_destroy :save_attributes_on_associated_members
+
+  private
+
+  def save_attributes_on_associated_members
+    members.each do |member|
+      member.update!(
+        display_name: member.display_name == name ? name : member.display_name,
+        email: email,
+        user_id: nil
+      )
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,24 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of :name }
+
+  describe '#destroy' do
+    subject(:user) { member.user }
+    let(:member) { FactoryGirl.create(:member) }
+
+    before do
+      user.destroy
+      member.reload
+    end
+
+    it 'bequeaths its name to associated members' do
+      expect(member.send(:read_attribute, :display_name))
+        .to eq(user.name)
+    end
+
+    it 'bequeaths its email to associated members' do
+      expect(member.send(:read_attribute, :email))
+        .to eq(user.email)
+    end
+  end
 end
