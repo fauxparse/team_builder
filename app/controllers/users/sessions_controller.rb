a1bf9559287a30eff008679466ffc6c06a5d9fe3
Matt Powell <fauxparse@gmail.com>
2016-03-13 22:23:35 +1300

    fix rubocop style violations

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,7 @@ class Users::SessionsController < Devise::SessionsController
   private
 
   def after_sign_in_path_for(resource_or_scope)
-    stored_location_for(:user) || signed_in_root_path(:user)
+    stored_location_for(resource_or_scope) ||
+      signed_in_root_path(resource_or_scope)
   end
 end
